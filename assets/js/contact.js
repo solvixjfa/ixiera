b21c1160ce735562a02ab26ebd59cdfc9e39518b
solvixjfa <jeffry4job@gmail.com>
2025-08-14 15:58:58 +0700

    Add portal-klien project to monorepo

--- assets/js/contact.js
+++
@@ -1,9 +0,0 @@
-// assets/js/contact.js
-document.addEventListener("DOMContentLoaded", function () {
-  const checkTallyLoaded = setInterval(() => {
-    if (typeof Tally !== "undefined" && Tally.loadEmbeds) {
-      Tally.loadEmbeds(); // Memuat ulang embed jika belum tampil
-      clearInterval(checkTallyLoaded);
-    }
-  }, 100); // cek tiap 100ms
-});
